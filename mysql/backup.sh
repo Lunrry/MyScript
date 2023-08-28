5b4b7264dd3a28ea7447b7826c9fd42238d56c00
Lunrry <creator_1999@163.com>
2023-08-28 11:51:10 +0800

    优化代码

--- mysql/backup.sh
+++ mysql/backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
  
 # 脚本存放位置即为备份文件存放位置
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/data/mysql/bin:/data/mysql/bin
 # 定义属性
 MYSQL_HOST="127.0.0.1"
  
@@ -16,11 +18,10 @@ echo "$BACKUP"
 DATETIME=$(date +%Y_%m_%d_%H_%M_%S)
 echo "$DATETIME"
 
-#此处没有优化，应该自动获取mysql和mysqldump绝对路径，which命令corntab无法使用
-#mysqldir=$(which mysql)
+mysqldir=$(which mysql)
 #echo "$mysqldir"
 
-#mysqldumpdir=$(which mysqldump)
+mysqldumpdir=$(which mysqldump)
 #echo "$mysqldumpdir"
  
 touch $BACKUP/backuplog.log
@@ -33,7 +34,7 @@ function log_correct () {
 log_correct "开始执行 Mysql 备份任务"
  
 # 自动获得所有的数据库
-DATABASES=`/data/mysql/bin/mysql -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "SHOW DATABASES;"`
+DATABASES=`${mysqldir} -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "SHOW DATABASES;"`
 # 创建备份目录
 mkdir -p "${BACKUP}/$DATETIME"
  
@@ -42,7 +43,7 @@ for db in $DATABASES; do
     if [[ "$db" != "Database" ]] && [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != "sys" ]] ; then
         # 备份操作
         log_correct "备份: 【$db】"
-        /data/mysql/bin/mysqldump --ignore-table=$db\.TS_S_OperationLog -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWORD} --databases $db > ${BACKUP}/$DATETIME/$db.sql
+        ${mysqldump} --ignore-table=$db\.TS_S_OperationLog -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWORD} --databases $db > ${BACKUP}/$DATETIME/$db.sql
         log_correct "数据库【$db】已备份到：${BACKUP}/$DATETIME/$db.tar.gz 下"
 	cd $BACKUP/$DATETIME
 	tar -czvf $db.tar.gz $db.sql
