a4d97b02992a280815ecdf5487f261eb7cf812b2
Lunrry <creator_1999@163.com>
2023-10-16 14:19:11 +0800

    Update changeip.sh

--- change_staticip/changeip.sh
+++ change_staticip/changeip.sh
@@ -9,10 +9,10 @@ echo -e "${RED}===============此命令会修改网卡静态ip地址============
 echo -e "${RED}===================仅适用当前机器=====================${NC}"
 echo -e "${RED}======================================================${NC}"
 echo "当前网卡信息为："
-cat /etc/sysconfig/network-scripts/ifcfg-ens32
+cat /etc/sysconfig/network-scripts/ifcfg-ens33
 
-sed -i 's/BOOTPROTO="none"/BOOTPROTO="static"/' /etc/sysconfig/network-scripts/ifcfg-ens32
-sed -i "s/IPADDR=\"192.168.0.3\"/IPADDR=\"192.168.0.${1}\"/" /etc/sysconfig/network-scripts/ifcfg-ens32
+sed -i 's/BOOTPROTO="none"/BOOTPROTO="static"/' /etc/sysconfig/network-scripts/ifcfg-ens33
+sed -i "s/IPADDR=\"192.168.0.3\"/IPADDR=\"192.168.0.${1}\"/" /etc/sysconfig/network-scripts/ifcfg-ens33
 
 
 sleep 3
